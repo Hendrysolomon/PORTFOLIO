1425ef8964cdef4d38f8b15e2b25097b1236640d
Hendrysolomon <solomonhendry123@gmail.com>
2024-10-19 01:16:47 -0700

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,12 @@
+var typing= new Typed(".texte",{
+    strings: ["Frontend Developer","Backend Developer","Full stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+
+})
+
+function aleart(){
+    alert("Your Feedback send succesufully thank you")
+}
